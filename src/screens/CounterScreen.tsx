dfd7d46f7fb6c819a01de66a695c4ca4214d810e
Friosn <franz.rios.nino@gmail.com>
2023-03-16 14:41:34 +0100

    StyleSheets added

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Counter} from '../components/Counter';
 
 const CounterScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'space-around',
-        backgroundColor: 'white',
-        borderRadius: 10,
-      }}>
+    <View style={styles.container}>
       <Counter />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+});
 
 export default CounterScreen;
